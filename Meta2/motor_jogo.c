cec8a8495a4e5fec726df638c09815f919a9f005
lucas <lucas@Ubuntu.myguest.virtualbox.org>
2023-12-03 10:59:33 +0000

    correção

--- Meta2/motor_jogo.c
+++ Meta2/motor_jogo.c
@@ -26,7 +26,40 @@ void enviaLabirinto() {
     fclose(file);
 }
 
+void lancaBot(){
+    pid_t pid = fork();
+
+    if (pid == -1) {
+        // Ocorreu um erro ao criar o processo
+        perror("fork");
+        exit(EXIT_FAILURE);
+    } else if (pid > 0) {
+        // Processo pai
+        printf("Processo pai: Esperando pelo término do processo filho...\n");
+
+        // Aguarda o término do processo filho
+        waitpid(pid, NULL, 0);
+
+        printf("Processo pai: Processo filho terminou.\n");
+    } else {
+        // Processo filho
+        printf("Processo filho: Executando o programa filho...\n");
+
+        // Redireciona a saída padrão para um arquivo
+        int novo_descritor = creat("saida.txt", 0644);
+        dup(novo_descritor);
+        close(novo_descritor);
+
+        // Substitua "seu_programa" pelo nome do programa que deseja executar
+        execlp("./bot", "./bot", "2", "3", (char *)NULL);
+
+        // Se o execlp() falhar, exibe uma mensagem de erro
+        perror("execlp");
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main(){
     enviaLabirinto();
+    lancaBot();
 }
